40403c3a9c6a33b0eecca1231aae4b0cca2c9e9d
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2023-03-20 15:33:30 -0500

    topology2: repurpose cavs-nocodec-bt as test vehicle for SSP
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- tools/topology/topology2/sof-ace-tplg/tplg-targets.cmake
+++ tools/topology/topology2/sof-ace-tplg/tplg-targets.cmake
@@ -3,6 +3,18 @@
 # Array of "input-file-name;output-file-name;comma separated pre-processor variables"
 set(TPLGS
 
-"cavs-sdw\;sof-lnl-rt5682\;PLATFORM=mtl,NUM_DMICS=0,NUM_HDMIS=0"
+  "cavs-sdw\;sof-lnl-rt5682\;PLATFORM=mtl,NUM_DMICS=0,NUM_HDMIS=0"
+
+  # CAVS SSP/DMIC topology for MTL/LNL
+
+
+"cavs-nocodec-bt\;sof-tgl-nocodec\;PLATFORM=tgl,NUM_DMICS=0,PDM1_MIC_A_ENABLE=1,PDM1_MIC_B_ENABLE=1,\
+PREPROCESS_PLUGINS=nhlt,NHLT_BIN=nhlt-sof-tgl-nocodec.bin"
+
+"cavs-nocodec-bt\;sof-mtl-nocodec\;PLATFORM=mtl,NUM_DMICS=0,PDM1_MIC_A_ENABLE=1,PDM1_MIC_B_ENABLE=1,\
+PREPROCESS_PLUGINS=nhlt,NHLT_BIN=nhlt-sof-mtl-nocodec.bin"
+
+"cavs-nocodec-bt\;sof-lnl-nocodec\;PLATFORM=mtl,NUM_DMICS=0,PDM1_MIC_A_ENABLE=1,PDM1_MIC_B_ENABLE=1,\
+PREPROCESS_PLUGINS=nhlt,NHLT_BIN=nhlt-sof-lnl-nocodec.bin"
 
 )
